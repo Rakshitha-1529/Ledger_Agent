a8be7dc20aa9a41087a6d1ee751fcfbb147e532a
Rakshitha_1529 <rakshithamolabanti@gmail.com>
2026-09-11 10:01:48 +0530

    final commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-router-dom";
 import "./styles.css";
 
-const API = "http://localhost:5000/api";
+const API = import.meta.env.VITE_API_URL;
 
 /* =========================================================
    API HELPER
